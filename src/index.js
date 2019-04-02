5dc275198dbe28b2534ddd5efd93eb16ec15cba4
terry-ice <326959013@qq.com>
2019-04-02 22:43:30 +0800

    mock

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import express from 'express';
-// import models from './models';
 import routes from './core/routes';
+import mock from './routes';
 import mongodb from './core/mongodb';
-import config from './config'
-
+import config from './config';
+import models from './models';
 
 const app = express();
 
@@ -21,13 +21,17 @@ app.set('port', config.APP.PORT);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-//   req.context = {
-//     models,
-//     me: models.users[1],
-//   };
-//   next();
-// });
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
+
+app.use('/session', mock.session);
+app.use('/users', mock.user);
+app.use('/messages', mock.message);
 
 // Routes
 routes(app);
@@ -35,5 +39,9 @@ routes(app);
 // Start
 
 app.listen(app.get('port'), () =>
-  console.log(`NodePress Run！port at ${app.get('port')}, env: ${process.env.NODE_ENV}`)
+  console.log(
+    `NodePress Run！port at ${app.get('port')}, env: ${
+      process.env.NODE_ENV
+    }`,
+  ),
 );
---
+++ src/models/index.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: terry
+ * @Date: 2019-04-02 22:43:12
+ * @Last Modified by:   https://github.com/terry-ice
+ * @Last Modified time: 2019-04-02 22:43:12
+ */
+let users = {
+  1: {
+    id: '1',
+    username: 'hello terry',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World terry or muze',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
