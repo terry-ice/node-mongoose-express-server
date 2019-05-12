0d09f1f86b7e128ca7bbd188680a8a676c39ab87
terry-ice <326959013@qq.com>
2019-05-12 23:06:29 +0800

    config key

--- src/config.js
+++ src/config.js
@@ -35,7 +35,7 @@ exports.EMAIL = {
 }
 
 exports.AKISMET = {
-	key: argv.akismet_key || 'c71123bcf0c7',
+	key: argv.akismet_key || 'akismet_key',
 	blog: argv.akismet_blog || 'your akismet blog site, like: '
 }
 
